968c3302c4dc24a4aef51769cff9045cdabbc39c
silvaguedes <aldairguedessilvao2gmail.com>
2024-05-01 07:39:32 -0300

    complementando o código com ferramenta reduce.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 const list = document.querySelector('ul')
 const cliquei = document.querySelector('.press')
 const buttonMap = document.querySelector('.press-map')
-
+const somaValue = document.querySelector('.soma-valor')
 
 
 function aoClicar(productsArray) {
@@ -38,9 +38,17 @@ function aoMapear() {
     aoClicar(newPrices)
 
 }
+function somaTudo() {
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+    list.innerHTML = `
+         <li>
+            <p> O valor total dos itens é : R$ ${totalValue} </p>
+        </li>
+         `
+}
 
 
-
+buttonMap.addEventListener('click', aoMapear)
 cliquei.addEventListener('click', () => aoClicar(menuOptions))
-
-buttonMap.addEventListener('click', aoMapear) 
+somaValue.addEventListener('click', somaTudo)
